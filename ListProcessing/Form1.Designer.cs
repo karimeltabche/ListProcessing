d228bdd8bb9391de6e2b44955d879580fcb19807
karimeltabche <karimeltabche24@gmail.com>
2016-05-25 12:41:44 -0400

    Add project files.

---
+++ ListProcessing/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace ListProcessing
+{
+    partial class frmListProcess
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnSort = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnHighest = new System.Windows.Forms.Button();
+            this.btnLowest = new System.Windows.Forms.Button();
+            this.btnTotal = new System.Windows.Forms.Button();
+            this.lstProcess = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnMean = new System.Windows.Forms.Button();
+            this.btnMode = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(398, 93);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(75, 23);
+            this.btnAdd.TabIndex = 0;
+            this.btnAdd.Text = "Add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Location = new System.Drawing.Point(398, 122);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(75, 23);
+            this.btnRemove.TabIndex = 1;
+            this.btnRemove.Text = "Remove";
+            this.btnRemove.UseVisualStyleBackColor = true;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnSort
+            // 
+            this.btnSort.Location = new System.Drawing.Point(398, 181);
+            this.btnSort.Name = "btnSort";
+            this.btnSort.Size = new System.Drawing.Size(75, 23);
+            this.btnSort.TabIndex = 2;
+            this.btnSort.Text = "Sort";
+            this.btnSort.UseVisualStyleBackColor = true;
+            this.btnSort.Click += new System.EventHandler(this.btnSort_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(398, 399);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 3;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnHighest
+            // 
+            this.btnHighest.Location = new System.Drawing.Point(398, 210);
+            this.btnHighest.Name = "btnHighest";
+            this.btnHighest.Size = new System.Drawing.Size(75, 23);
+            this.btnHighest.TabIndex = 4;
+            this.btnHighest.Text = "Highest";
+            this.btnHighest.UseVisualStyleBackColor = true;
+            this.btnHighest.Click += new System.EventHandler(this.btnHighest_Click);
+            // 
+            // btnLowest
+            // 
+            this.btnLowest.Location = new System.Drawing.Point(398, 239);
+            this.btnLowest.Name = "btnLowest";
+            this.btnLowest.Size = new System.Drawing.Size(75, 23);
+            this.btnLowest.TabIndex = 5;
+            this.btnLowest.Text = "Lowest";
+            this.btnLowest.UseVisualStyleBackColor = true;
+            this.btnLowest.Click += new System.EventHandler(this.btnLowest_Click);
+            // 
+            // btnTotal
+            // 
+            this.btnTotal.Location = new System.Drawing.Point(398, 353);
+            this.btnTotal.Name = "btnTotal";
+            this.btnTotal.Size = new System.Drawing.Size(75, 23);
+            this.btnTotal.TabIndex = 6;
+            this.btnTotal.Text = "Total";
+            this.btnTotal.UseVisualStyleBackColor = true;
+            this.btnTotal.Click += new System.EventHandler(this.btnTotal_Click);
+            // 
+            // lstProcess
+            // 
+            this.lstProcess.FormattingEnabled = true;
+            this.lstProcess.Location = new System.Drawing.Point(12, 38);
+            this.lstProcess.Name = "lstProcess";
+            this.lstProcess.Size = new System.Drawing.Size(284, 381);
+            this.lstProcess.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(373, 38);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 8;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(398, 64);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(75, 23);
+            this.btnSearch.TabIndex = 9;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnMean
+            // 
+            this.btnMean.Location = new System.Drawing.Point(398, 268);
+            this.btnMean.Name = "btnMean";
+            this.btnMean.Size = new System.Drawing.Size(75, 23);
+            this.btnMean.TabIndex = 10;
+            this.btnMean.Text = "Mean";
+            this.btnMean.UseVisualStyleBackColor = true;
+            this.btnMean.Click += new System.EventHandler(this.btnMean_Click);
+            // 
+            // btnMode
+            // 
+            this.btnMode.Location = new System.Drawing.Point(398, 298);
+            this.btnMode.Name = "btnMode";
+            this.btnMode.Size = new System.Drawing.Size(75, 23);
+            this.btnMode.TabIndex = 11;
+            this.btnMode.Text = "Mode";
+            this.btnMode.UseVisualStyleBackColor = true;
+            this.btnMode.Click += new System.EventHandler(this.btnMode_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(485, 24);
+            this.menuStrip1.TabIndex = 12;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.openToolStripMenuItem.Text = "Open";
+            this.openToolStripMenuItem.Click += new System.EventHandler(this.openToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.saveToolStripMenuItem.Text = "Save";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.Filter = "\"Text files (*.txt)|*.txt|All files (*.*)|*.*\"";
+            // 
+            // frmListProcess
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(485, 437);
+            this.Controls.Add(this.btnMode);
+            this.Controls.Add(this.btnMean);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.lstProcess);
+            this.Controls.Add(this.btnTotal);
+            this.Controls.Add(this.btnLowest);
+            this.Controls.Add(this.btnHighest);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnSort);
+            this.Controls.Add(this.btnRemove);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmListProcess";
+            this.Text = "Total ";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnSort;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnHighest;
+        private System.Windows.Forms.Button btnLowest;
+        private System.Windows.Forms.Button btnTotal;
+        private System.Windows.Forms.ListBox lstProcess;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnMean;
+        private System.Windows.Forms.Button btnMode;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+    }
+}
+
---
+++ ListProcessing/Form1.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace ListProcessing
+{
+    public partial class frmListProcess : Form
+    {
+        List<int> list = new List<int>();
+
+        public frmListProcess()
+        {
+            InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            StreamReader inputFile;
+
+            openFileDialog1.InitialDirectory = "H:\\";
+            openFileDialog1.Title = "Open File";
+            openFileDialog1.FileName = "";
+            int num;
+
+            list.Clear();
+            lstProcess.Items.Clear();
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+
+                    inputFile = File.OpenText(openFileDialog1.FileName);
+
+                    while (!inputFile.EndOfStream)
+                    {
+                        num = int.Parse(inputFile.ReadLine());
+                        lstProcess.Items.Add(num);
+                        list.Add(num);
+                    }
+                    inputFile.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            int search = int.Parse(textBox1.Text);
+            int count = 0;
+            int index = list.IndexOf(search);
+
+            if (index == -1)
+            {
+                MessageBox.Show("Item not found.");
+               
+            }
+            else
+            {
+                do
+                {
+                    count += 1;
+                    index = list.IndexOf(search, index + 1);
+
+                } while (index != -1);
+
+                MessageBox.Show("Found the number " + textBox1.Text + "\nTimes found: " + count.ToString());
+
+            }
+
+        }
+
+        private void btnSort_Click(object sender, EventArgs e)
+        {
+            lstProcess.Items.Clear();
+
+            list.Sort();
+
+            foreach(int x in list)
+            {
+                lstProcess.Items.Add(x);
+            }
+        }
+
+        private void btnHighest_Click(object sender, EventArgs e)
+        {
+                        
+            int max = list.Max();
+
+            MessageBox.Show("Highest value is " + max.ToString());
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            int x = int.Parse(textBox1.Text);
+            lstProcess.Items.Remove(textBox1.Text);
+            list.Remove(x);
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            int outParse;
+            int x;
+            x = int.Parse(textBox1.Text);
+
+            if(Int32.TryParse(textBox1.Text, out outParse))
+            {
+                lstProcess.Items.Add(textBox1.Text);
+                list.Add(x);
+            }
+            else if(textBox1.Text == "")
+            {
+                MessageBox.Show("Please enter a numeric value to add to the list");
+            }
+            else
+            {
+                MessageBox.Show("Please enter a numeric value to add to the list");
+            }
+        }
+
+        private void btnTotal_Click(object sender, EventArgs e)
+        {
+
+            int i = 0, result = 0;
+            while (i < list.Count)
+            {
+                result += Convert.ToInt32(lstProcess.Items[i++]);
+            }
+            MessageBox.Show("The total value is " + result.ToString(), "Total");
+        }
+
+        private void btnLowest_Click(object sender, EventArgs e)
+        {
+
+            int min = list.Min();
+
+            MessageBox.Show("Lowest value is " + min.ToString());
+        }
+
+        private void btnMean_Click(object sender, EventArgs e)
+        {
+            double mean = 0;
+            int i = 0;
+            double total = 0;
+            while (i < list.Count)
+            {
+                total += Convert.ToInt32(lstProcess.Items[i++]);
+            }
+
+            mean = total / lstProcess.Items.Count;
+            MessageBox.Show("The mean is " + mean.ToString());
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.InitialDirectory = "H:\\";
+            saveFileDialog1.Title = "Save File";
+            saveFileDialog1.FileName = "";
+            saveFileDialog1.ShowDialog();
+
+            if (saveFileDialog1.FileName != "")
+            {
+                System.IO.StreamWriter sw = new System.IO.StreamWriter(saveFileDialog1.FileName);
+
+                foreach (object item in list)
+                {
+                    sw.WriteLine(item.ToString());
+                }
+
+                sw.Close();
+            }
+        }
+
+        private void btnMode_Click(object sender, EventArgs e)
+        {
+            int mode = list.GroupBy(v => v)
+                        .OrderByDescending(g => g.Count())
+                        .First()
+                        .Key;
+            MessageBox.Show("The mode is " + mode.ToString());
+        }
+    }
+}
